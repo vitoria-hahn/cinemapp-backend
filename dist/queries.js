bea746ef82fab133970a39ac24f047e8213fe5e6
vitoria-hahn <vitoria.ebeling@edu.pucrs.br>
2023-08-10 16:54:30 -0300

    Database creation and config.

---
+++ dist/queries.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const connectDb = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const client = new pg_1.Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: 5432,
+        });
+        yield client.connect();
+        const res = yield client.query("SELECT * FROM favoritos");
+        console.log(res);
+        yield client.end();
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+connectDb();
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
 
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 
 const app = express();
 
-const route = Router()
+const route = Router();
 
-app.use(express.json())
+app.use(express.json());
 
-route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript' })
-})
+dotenv.config();
 
-app.use(route)
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world with Typescript" });
+});
 
+app.use(route);
 
-app.listen(3333, () => 'server running on port 3333')
+app.listen(3333, () => "server running on port 3333");
