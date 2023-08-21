e055479bc843eb950f136b4d94446737e7ed0de4
vitoria-hahn <vitoria.ebeling@edu.pucrs.br>
2023-08-21 16:46:11 -0300

    Allows usage of filter.

--- src/api/repositories/MoviesRepository.ts
+++ src/api/repositories/MoviesRepository.ts
@@ -6,7 +6,7 @@ interface MoviesRepository {
 
   getById(id: string): Promise<Movie>;
 
-  getAll(startIndex: number, endIndex: number): Promise<GetAllResponse>;
+  getAll(startIndex: number, endIndex: number, column?: string, value?: string): Promise<GetAllResponse>;
 
   delete(id: string): Promise<number>;
 }
