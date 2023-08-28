51f5fd99bbbb3ec234a6b78763104ac10f811163
vitoria-hahn <vitoria.ebeling@edu.pucrs.br>
2023-08-28 11:58:21 -0300

    Validates column filter.

--- src/api/controllers/utils/Query.ts
+++ src/api/controllers/utils/Query.ts
@@ -4,16 +4,24 @@ interface Filter {
     operator: string;
 }
 
+const validColumns = ["genre", "imdbScore", "title", "director", "year"];
+
 export function buildSqlRawSelectQuery(tableName: string, limit: number, offset: number, filters?: Filter[]): string {
-    let query = `SELECT * FROM ${tableName}`;
+    const alias = "movie";
+
+    let query = `SELECT * FROM ${tableName} as ${alias}`;
 
     if (filters && filters.length > 0) {
         query += ' WHERE ';
         const filterConditions = filters.map(filter => {
-            if (filter.field === "genre") {
-                return `'${filter.value}' = ANY (${tableName}.${filter.field})`;
+            if (validColumns.includes(filter.field)) {
+                if (filter.field === "genre") {
+                    return `'${filter.value}' = ANY (${alias}.${filter.field})`;
+                } else {
+                    return `${filter.field} ${filter.operator} ${filter.value}`;
+                }
             } else {
-                return `${filter.field} ${filter.operator} ${filter.value}`;
+                throw SyntaxError("invalid column to filter");
             }
         });
 
@@ -26,13 +34,15 @@ export function buildSqlRawSelectQuery(tableName: string, limit: number, offset:
 }
 
 export function buildSqlRawCountQuery(tableName: string, filters?: Filter[]): string {
-    let query = `SELECT COUNT(*) as total FROM ${tableName}`;
+    const alias = "movie";
+
+    let query = `SELECT COUNT(*) as total FROM ${tableName} AS ${alias}`;
 
     if (filters && filters.length > 0) {
         query += ' WHERE ';
         const filterConditions = filters.map(filter => {
             if (filter.field === "genre") {
-                return `'${filter.value}' = ANY (${tableName}.${filter.field})`;
+                return `'${filter.value}' = ANY (${alias}.${filter.field})`;
             } else {
                 return `${filter.field} ${filter.operator} ${filter.value}`;
             }
