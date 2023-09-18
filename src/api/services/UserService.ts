fbaee95f4984f82ef4ea039a014a969faa03e593
vitoria-hahn <vitoria.ebeling@edu.pucrs.br>
2023-09-18 13:48:38 -0300

    token epires in 1 hour.

--- src/api/services/UserService.ts
+++ src/api/services/UserService.ts
@@ -70,7 +70,7 @@ export class UserService {
 
           const signOptions: SignOptions = {
             algorithm: "HS256",
-            expiresIn: "10s",
+            expiresIn: "1h",
           };
 
           const token = generateJWT(payload, signOptions, secretKey);
